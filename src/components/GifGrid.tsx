0d86cf79e5550dd956391cf6d206db7f98a5a91b
Buty06 <acnielwilliams08@gmail.com>
2025-10-06 19:51:25 -0400

    Agregando los gif y sus imagenes con el useEffet

--- src/components/GifGrid.tsx
+++ src/components/GifGrid.tsx
@@ -1,16 +1,40 @@
+import { useEffect, useState } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 interface GifGridProps {
   category: string;
 }
 
+interface ImgeData {
+  id: string;
+  title: string;
+  url: string;
+}
+
 export const GifGrid: React.FC<GifGridProps> = ({ category }) => {
-  getGifs(category);
+  const [images, setImages] = useState([]);
+
+  const getImages = async () => {
+    const newImages = await getGifs(category);
+    setImages(newImages);
+  };
+
+  useEffect(() => {
+    getImages();
+  }, []);
 
   return (
     <>
       <h3> {category} </h3>
-      <p>Hola Mundo</p>
+
+      <ol>
+        {images.map(({ id, title, url }: ImgeData) => (
+          <li key={id}>
+            <h2> {title} </h2>
+            <img src={url} alt="image" />
+          </li>
+        ))}
+      </ol>
     </>
   );
 };
--- src/helpers/getGifs.ts
+++ src/helpers/getGifs.ts
@@ -9,7 +9,7 @@ interface GifData {
 }
 
 export const getGifs = async (category: string) => {
-  const url = `https://api.giphy.com/v1/stickers/search?api_key=wQ2HPsYOlmZrMDSlXIg4N6qcqObd182n&q=${category}&limit=20&offset=0&rating=g&lang=es&bundle=messaging_non_clips`;
+  const url = `https://api.giphy.com/v1/stickers/search?api_key=wQ2HPsYOlmZrMDSlXIg4N6qcqObd182n&q=${category}&limit=10&offset=0&rating=g&lang=es&bundle=messaging_non_clips`;
 
   const response = await fetch(url);
   const { data } = await response.json();
@@ -20,6 +20,5 @@ export const getGifs = async (category: string) => {
     url: gif.images.fixed_height_downsampled.url,
   }));
 
-  console.log(gifs);
   return gifs;
 };
